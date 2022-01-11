b5833dc1d6e7aafeeec323596e2f7beef486b171
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-01-11 23:18:25 +0530

    Update **trust.java

--- **trust.java
+++ **trust.java
@@ -1,6 +1,6 @@
 /*In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
 
-If the town judge exists, then:
+If the town judge exists, then   :
 
 The town judge trusts nobody.
 Everybody (except for the town judge) trusts the town judge.
