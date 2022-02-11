8bfae75d2dc62ef3c9cf3f404baf14c172caa5d1
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-02-11 21:51:27 +0530

    Update **trust.java

--- **trust.java
+++ **trust.java
@@ -5,7 +5,7 @@ If the town judge exists, then   :
 The town judge trusts nobody.
 Everybody (except for the town judge) trusts the town judge.
 There is exactly one person that satisfies properties 1 and 2.
-You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi.
+You are given an array trust where trust[i] = [ai, bi] representing that the person  labeled ai trusts the person labeled bi.
 
 Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
 
