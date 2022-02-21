7d48b4d627ed19da9a6144a5d9d1abe8c277145d
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-02-21 22:53:22 +0530

    Update 3rd_max.java

--- 3rd_max.java
+++ 3rd_max.java
@@ -9,7 +9,7 @@ Example 1:
 Input: nums = [3,2,1]
 Output: 1
 Explanation:
-The first distinct maximum is 3.
+The first distin ct maximum is 3.
 The second distinct maximum is 2.
 The third distinct maximum is 1.
 Example 2:
