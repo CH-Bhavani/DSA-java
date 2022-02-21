4aad31ca541ae17ecb3c483a8cf9ca879261be78
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-02-21 22:53:13 +0530

    Update 3rd_max.java

--- 3rd_max.java
+++ 3rd_max.java
@@ -26,7 +26,7 @@ Input: nums = [2,2,3,1]
 Output: 1
 Explanation:
 The first distinct maximum is 3.
-The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The second distinct maximum is  2 (both 2's are counted together since they have the same value).
 The third distinct maximum is 1.
 
 
