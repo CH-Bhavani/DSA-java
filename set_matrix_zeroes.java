603101a800115a6eab60ea6426c6c0d6dccb6bec
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-03-13 18:44:48 +0530

    Create set_matrix_zeroes.java

---
+++ set_matrix_zeroes.java
@@ -0,0 +1,47 @@
+/* Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]] */
+
+
+
+
+class Solution {
+  public void setZeroes(int[][] matrix) {
+    int R = matrix.length;
+    int C = matrix[0].length;
+    Set<Integer> rows = new HashSet<Integer>();
+    Set<Integer> cols = new HashSet<Integer>();
+
+    // Essentially, we mark the rows and columns that are to be made zero
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (matrix[i][j] == 0) {
+          rows.add(i);
+          cols.add(j);
+        }
+      }
+    }
+
+    // Iterate over the array once again and using the rows and cols sets, update the elements.
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (rows.contains(i) || cols.contains(j)) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
+}
