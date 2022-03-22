170bf60cfbe1dd973117c7609b76d3994f5707c0
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-03-22 20:48:23 +0530

    Create  Positions of Large Groups.java

---
+++ Positions of Large Groups.java
@@ -0,0 +1,49 @@
+/*
+In a string s of lowercase letters, these letters form consecutive groups of the same character.
+
+For example, a string like s = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z", and "yy".
+
+A group is identified by an interval [start, end], where start and end denote the start and end indices (inclusive) of the group. In the above example, "xxxx" has the interval [3,6].
+
+A group is considered large if it has 3 or more characters.
+
+Return the intervals of every large group sorted in increasing order by start index.
+
+ 
+
+Example 1:
+
+Input: s = "abbxxxxzzy"
+Output: [[3,6]]
+Explanation: "xxxx" is the only large group with start index 3 and end index 6.
+Example 2:
+
+Input: s = "abc"
+Output: []
+Explanation: We have groups "a", "b", and "c", none of which are large groups.
+Example 3:
+
+Input: s = "abcdddeeeeaabbbcd"
+Output: [[3,5],[6,9],[12,14]]
+Explanation: The large groups are "ddd", "eeee", and "bbb".
+
+*/
+
+
+
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String S) {
+        List<List<Integer>> ans = new ArrayList();
+        int i = 0, N = S.length(); // i is the start of each group
+        for (int j = 0; j < N; ++j) {
+            if (j == N-1 || S.charAt(j) != S.charAt(j+1)) {
+                // Here, [i, j] represents a group.
+                if (j-i+1 >= 3)
+                    ans.add(Arrays.asList(new Integer[]{i, j}));
+                i = j + 1;
+            }
+        }
+
+        return ans;
+    }
+}
