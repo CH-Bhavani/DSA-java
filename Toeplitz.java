bc2694228a9f3e0b5147e3513c83d2393afc2494
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-04-24 15:05:00 +0530

    Create Toeplitz.java

---
+++ Toeplitz.java
@@ -0,0 +1,64 @@
+/*
+
+
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.
+
+*/
+
+
+
+//sol-1
+
+
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(i>0 && j>0 && matrix[i-1][j-1]!=matrix[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+//sol-2
+
+
+
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        Map<Integer, Integer> groups = new HashMap();
+        for (int r = 0; r < matrix.length; ++r) {
+            for (int c = 0; c < matrix[0].length; ++c) {
+                if (!groups.containsKey(r-c))
+                    groups.put(r-c, matrix[r][c]);
+                else if (groups.get(r-c) != matrix[r][c])
+                    return False;
+            }
+        }
+        return True;
+    }
+}
