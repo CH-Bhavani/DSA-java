e56c08a9958322f941055f1e56856ef3607b1df7
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-05-16 20:37:01 +0530

    Create RangeAddition.java

---
+++ RangeAddition.java
@@ -0,0 +1,39 @@
+/*
+
+
+You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
+
+Count and return the number of maximum integers in the matrix after performing all the operations.
+
+ 
+
+Example 1:
+
+
+Input: m = 3, n = 3, ops = [[2,2],[3,3]]
+Output: 4
+Explanation: The maximum integer in M is 2, and there are four of it in M. So return 4.
+Example 2:
+
+Input: m = 3, n = 3, ops = [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3]]
+Output: 4
+Example 3:
+
+Input: m = 3, n = 3, ops = []
+Output: 9
+
+
+*/
+
+
+class Solution {
+    public int maxCount(int k, int l, int[][] ops) {
+        int m=k,n=l,count=k*l;
+        for(int[] arr:ops){
+            m=Math.min(m,arr[0]);
+            n=Math.min(n,arr[1]);
+            count=Math.min(count,m*n);
+        }
+        return count;
+    }
+}
