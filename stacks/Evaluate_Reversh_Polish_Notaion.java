a7d5b171c56679c4387278d092c57d40d3c01e83
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-06-04 11:48:33 +0530

    Create Evaluate_Reversh_Polish_Notaion.java

---
+++ stacks/Evaluate_Reversh_Polish_Notaion.java
@@ -0,0 +1,73 @@
+/*Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+Note that division between two integers should truncate toward zero.
+
+It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+
+
+*/
+
+
+
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String token : tokens) {
+            if (!isOperator(token)) {
+                stack.push(Integer.valueOf(token));
+            } else {
+                int num1 = stack.pop(); 
+                int num2 = stack.pop(); 
+                int res = 0;
+                switch(token) {
+                    case "+":
+                        res = num2 + num1;
+                        break;
+                    case "-":
+                        res = num2 - num1;
+                        break;
+                    case "*":
+                        res = num2 * num1;
+                        break;
+                    case "/":
+                        res = num2 / num1;
+                        break;
+                }
+                stack.push(res);
+            }
+        }
+        return stack.peek();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+}
