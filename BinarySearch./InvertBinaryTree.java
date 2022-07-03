2bf751339b5f72bcd9d513b55069b237d0a4e126
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-07-03 11:20:48 +0530

    Create InvertBinaryTree.java

---
+++ BinarySearch./InvertBinaryTree.java
@@ -0,0 +1,90 @@
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)
+            return null;
+        TreeNode right=invertTree(root.right);
+        TreeNode left =invertTree(root.left);
+        root.left=right;
+        root.right=left;
+        return root;
+    }
+}
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)
+            return root;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode cur=q.poll();
+            TreeNode temp=cur.left;
+            cur.left=cur.right;
+            cur.right=temp;
+            if(cur.left!=null)
+                q.offer(cur.left);
+            if(cur.right!=null)
+                q.offer(cur.right);
+        }
+        return root;
+        
+    }
+}
