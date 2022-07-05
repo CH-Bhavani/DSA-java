1403f87120bac66053dd12a20d82fd62e4173148
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-07-05 09:52:52 +0530

    Create Longest Consecutive Sequence.java

---
+++ Sequenece/Longest Consecutive Sequence.java
@@ -0,0 +1,110 @@
+/*
+
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+
+
+*/
+
+o(n)-sol
+
+
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> hs=new HashSet<>();
+        for(int num:nums)
+            hs.add(num);
+        int max=0;
+        for(int num:nums){
+            if(!hs.contains(num-1)){
+                int curnum=num;
+                int curmax=1;
+                while(hs.contains(curnum+1)){
+                    curnum++;
+                    curmax++;
+                }
+                max=Math.max(max,curmax);
+            }
+        }
+        return max;
+    }
+}
+
+
+
+o(nlogn)-sol
+
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int longestStreak = 1;
+        int currentStreak = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                if (nums[i] == nums[i-1]+1) {
+                    currentStreak += 1;
+                }
+                else {
+                    longestStreak = Math.max(longestStreak, currentStreak);
+                    currentStreak = 1;
+                }
+            }
+        }
+
+        return Math.max(longestStreak, currentStreak);
+    }
+}
+
+
+o(n2)-sol
+
+class Solution {
+    private boolean arrayContains(int[] arr, int num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public int longestConsecutive(int[] nums) {
+        int longestStreak = 0;
+
+        for (int num : nums) {
+            int currentNum = num;
+            int currentStreak = 1;
+
+            while (arrayContains(nums, currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+
+        return longestStreak;
+    }
+}
