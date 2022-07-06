abdadbb704b88dd082fc357968e19df2f69954bf
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-07-06 20:11:30 +0530

    Update FibionacciSeries.java

--- DP/FibionacciSeries.java
+++ DP/FibionacciSeries.java
@@ -1,3 +1,33 @@
+/*
+
+he Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+*/
+
+
+
 import java.util.HashMap;
 
 public class Main {
@@ -19,3 +49,24 @@ public class Main {
     return result;
   }
 }
+
+
+
+
+
+class Solution {
+    int dp[];
+    public int fib(int n) {
+        dp=new int[n+1];
+        return calculate(n);
+    }
+    public int calculate(int n){
+        if(n<=1)
+            return dp[n]=n;
+        if(dp[n]!=0)
+            return dp[n];
+        return dp[n]=calculate(n-1)+calculate(n-2);
+        
+    }
+}
+
