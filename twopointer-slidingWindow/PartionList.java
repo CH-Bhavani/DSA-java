3c7d690db8c50b20c44248a36cf10322180f3e99
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-07-22 19:07:38 +0530

    Update PartionList.java

--- twopointer-slidingWindow/PartionList.java
+++ twopointer-slidingWindow/PartionList.java
@@ -29,7 +29,11 @@ Output: [1,2]
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ 
  */
+
+
+
 class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode bhead=new ListNode(0);
