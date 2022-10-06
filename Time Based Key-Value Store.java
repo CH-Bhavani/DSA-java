e52130980f6e1e4dc88fbc4601271dda57d220df
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-10-06 05:55:41 -0500

    Update Time Based Key-Value Store.java

--- Time Based Key-Value Store.java
+++ Time Based Key-Value Store.java
@@ -70,3 +70,45 @@ class TimeMap {
  * String param_2 = obj.get(key,timestamp);
  */
 
+
+
+
+
+
+
+// sol2
+
+
+
+class TimeMap {
+     Map<String,TreeMap<Integer,String>> hm;
+    public TimeMap() {
+       hm=new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!hm.containsKey(key)){
+            hm.put(key,new TreeMap<Integer,String>());
+        }
+        hm.get(key).put(timestamp,value);
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!hm.containsKey(key))
+            return "";
+        Integer k=hm.get(key).floorKey(timestamp);
+        if(k!=null)
+            return hm.get(key).get(k);
+        return "";
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
+
+
+
