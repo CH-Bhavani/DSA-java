c50f7886abbaa5b75f86f510f7367d2bdd5df6a6
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-10-06 08:00:29 -0500

    Update AddRowToTree.java

--- Binary_trees/AddRowToTree.java
+++ Binary_trees/AddRowToTree.java
@@ -84,3 +84,65 @@ class Solution {
             
     }
 }
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Node{
+            TreeNode node;
+            int depth;
+            Node(TreeNode n,int d){
+                node=n;
+                depth=d;
+            }
+    }
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(depth==1){
+            TreeNode t=new TreeNode(val);
+            t.left=root;
+            return t;
+            
+        }
+        sol(root,val,depth);
+        return root;
+    }
+    public void sol(TreeNode root, int val, int d){
+        Stack<Node> stk=new Stack<>();
+        stk.push(new Node(root,1));
+        while(!stk.isEmpty()){
+            Node n=stk.pop();
+            if(n.node==null)
+                continue;
+            if(n.depth==d-1){
+                TreeNode temp=n.node.left;
+                n.node.left=new TreeNode(val);
+                n.node.left.left=temp;
+                temp=n.node.right;
+                n.node.right=new TreeNode(val);;
+                n.node.right.right=temp;
+
+            }
+            else{
+               stk.push(new Node(n.node.left,n.depth+1));
+                stk.push(new Node(n.node.right,n.depth+1));
+            }
+        }     
+    }
+}
