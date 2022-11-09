ab3db0dd1d067763c6fe70d73674e9d6873a7b8e
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-11-09 00:22:54 -0600

    Update MakeGoodString.java

--- stacks/MakeGoodString.java
+++ stacks/MakeGoodString.java
@@ -59,3 +59,26 @@ class Solution {
         return sb.reverse().toString();
     }
 }
+
+
+
+
+
+
+
+
+    public String makeGood(String s) {
+      if (s.length() <= 1) {
+        return s;
+      }
+      StringBuilder sb = new StringBuilder();
+      sb.append(s.charAt(0));
+      for (int i = 1; i < s.length(); i++) {
+        if (sb.length() > 0 && Math.abs(sb.charAt(sb.length() - 1) - s.charAt(i))==32) {
+          sb.deleteCharAt(sb.length() - 1);
+        } else {
+          sb.append(s.charAt(i));
+        }
+      }
+      return sb.toString();
+    }
