d426b03db0f74982fc99ba8df1c6b507e8709847
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2023-07-20 13:39:50 +0530

    Create Asteroid Collision.java

---
+++ stacks/Asteroid Collision.java
@@ -0,0 +1,72 @@
+/*
+
+We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+ 
+
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+ 
+
+Constraints:
+
+2 <= asteroids.length <= 104
+-1000 <= asteroids[i] <= 1000
+asteroids[i] != 0
+
+  */
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stk =new Stack<>();
+
+        for(int a:asteroids){
+            if(stk.isEmpty()||a>0)
+                stk.push(a);
+            else{
+                while(true){
+                    int peek=stk.peek();
+                    if(peek<0){
+                        stk.push(a);
+                        break;
+                    }
+                    else if (peek==-a){
+                        stk.pop();
+                        break;
+                    }
+                    else if (peek>-a)
+                        break;
+                    else{
+                        stk.pop();
+                        if(stk.isEmpty()){
+                            stk.push(a);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int[] res=new int[stk.size()];
+        for(int j=stk.size()-1;j>=0;j--){
+            res[j]=stk.pop();
+        }
+        return res;
+    }
+}
